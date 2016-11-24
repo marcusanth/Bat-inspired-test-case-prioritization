1ef15a3cb8483b65ca395102c884c14e2dfc1643
marcusanth <muhammedozturk@sakarya.edu.tr>
2016-11-24 09:41:56 +0300

    Add files via upload

---
+++ swarm.Designer.cs
@@ -0,0 +1,236 @@
+﻿namespace batAlgorithm
+{
+    partial class swarm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.minTextBox = new System.Windows.Forms.TextBox();
+            this.exitTextBox = new System.Windows.Forms.TextBox();
+            this.epochTextBox = new System.Windows.Forms.TextBox();
+            this.numTextBox = new System.Windows.Forms.TextBox();
+            this.dimTextBox = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.solutionTextBox = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(179, 271);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "swarm";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.Paint += new System.Windows.Forms.PaintEventHandler(this.button1_Paint);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.minTextBox);
+            this.groupBox1.Controls.Add(this.exitTextBox);
+            this.groupBox1.Controls.Add(this.epochTextBox);
+            this.groupBox1.Controls.Add(this.numTextBox);
+            this.groupBox1.Controls.Add(this.dimTextBox);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(335, 144);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(327, 166);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // minTextBox
+            // 
+            this.minTextBox.Location = new System.Drawing.Point(133, 132);
+            this.minTextBox.Name = "minTextBox";
+            this.minTextBox.Size = new System.Drawing.Size(100, 20);
+            this.minTextBox.TabIndex = 9;
+            // 
+            // exitTextBox
+            // 
+            this.exitTextBox.Location = new System.Drawing.Point(133, 109);
+            this.exitTextBox.Name = "exitTextBox";
+            this.exitTextBox.Size = new System.Drawing.Size(100, 20);
+            this.exitTextBox.TabIndex = 8;
+            // 
+            // epochTextBox
+            // 
+            this.epochTextBox.Location = new System.Drawing.Point(133, 86);
+            this.epochTextBox.Name = "epochTextBox";
+            this.epochTextBox.Size = new System.Drawing.Size(100, 20);
+            this.epochTextBox.TabIndex = 7;
+            // 
+            // numTextBox
+            // 
+            this.numTextBox.Location = new System.Drawing.Point(133, 60);
+            this.numTextBox.Name = "numTextBox";
+            this.numTextBox.Size = new System.Drawing.Size(100, 20);
+            this.numTextBox.TabIndex = 6;
+            // 
+            // dimTextBox
+            // 
+            this.dimTextBox.Location = new System.Drawing.Point(133, 22);
+            this.dimTextBox.Name = "dimTextBox";
+            this.dimTextBox.Size = new System.Drawing.Size(100, 20);
+            this.dimTextBox.TabIndex = 5;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(19, 139);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(43, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "minMax";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(17, 112);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(72, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "earyl exit error";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(17, 83);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "epoch";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(17, 60);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "numpArticles";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(17, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Probem dimension";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(64, 271);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "start";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(36, 124);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(69, 13);
+            this.label6.TabIndex = 3;
+            this.label6.Text = "Best Solution";
+            // 
+            // solutionTextBox
+            // 
+            this.solutionTextBox.Location = new System.Drawing.Point(39, 144);
+            this.solutionTextBox.Multiline = true;
+            this.solutionTextBox.Name = "solutionTextBox";
+            this.solutionTextBox.Size = new System.Drawing.Size(100, 73);
+            this.solutionTextBox.TabIndex = 4;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(179, 144);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 73);
+            this.textBox1.TabIndex = 5;
+            // 
+            // swarm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(674, 322);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.solutionTextBox);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "swarm";
+            this.Text = "swarm";
+            this.Load += new System.EventHandler(this.swarm_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.swarm_Paint);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox dimTextBox;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox numTextBox;
+        private System.Windows.Forms.TextBox minTextBox;
+        private System.Windows.Forms.TextBox exitTextBox;
+        private System.Windows.Forms.TextBox epochTextBox;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox solutionTextBox;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
